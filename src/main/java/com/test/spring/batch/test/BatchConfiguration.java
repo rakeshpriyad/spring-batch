109b2956c5e63ea2d2786fb04e1722c4de0c2076
rakesh <rakeshpriyad@gmail.com>
2017-09-09 14:43:27 +0530

    first commit

--- src/main/java/com/test/spring/batch/test/BatchConfiguration.java
+++ src/main/java/com/test/spring/batch/test/BatchConfiguration.java
@@ -16,7 +16,6 @@ import org.springframework.batch.item.file.transform.FieldSet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 
