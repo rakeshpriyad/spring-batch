10da9ac8d047e77e01cc0e8f6948740e560078d0
rakesh <rakeshpriyad@gmail.com>
2017-09-10 11:53:41 +0530

    test

--- src/main/java/com/test/spring/batch/test/Application.java
+++ src/main/java/com/test/spring/batch/test/Application.java
@@ -1,7 +1,7 @@
 package com.test.spring.batch.test;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //@SpringBootApplication
 public class Application {
