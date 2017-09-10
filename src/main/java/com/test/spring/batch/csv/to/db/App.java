c67fb07521d1815cc33ac608acc7930f67f03a7f
rakesh <rakeshpriyad@gmail.com>
2017-09-10 11:27:39 +0530

    test

---
+++ src/main/java/com/test/spring/batch/csv/to/db/App.java
@@ -0,0 +1,38 @@
+package com.test.spring.batch.csv.to.db;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+
+		String[] springConfig  = 
+			{	"database.xml", 
+				"context.xml",
+				"spring-batch-job-csv-to-db.xml" 
+			};
+		
+		ConfigurableApplicationContext context = 
+				new ClassPathXmlApplicationContext(springConfig);
+		
+		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+		Job job = (Job) context.getBean("reportJob");
+
+		try {
+
+			JobExecution execution = jobLauncher.run(job, new JobParameters());
+			System.out.println("Exit Status : " + execution.getStatus());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		context.close();
+		System.out.println("Done");
+
+	}
+}
